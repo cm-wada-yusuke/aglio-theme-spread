7f3c79e0f01fc37db6296320be8aa3e0fbbf8022
Yusuke Wada <wada.yusuke@classmethod.jp>
2018-03-28 14:31:48 +0900

    show all by default.

--- templates/scripts.js
+++ templates/scripts.js
@@ -184,10 +184,8 @@ function init() {
     for (i = 0; i < buttons.length; i++) {
         buttons[i].onclick = toggleCollapseButton;
 
-        // Show by default? Then toggle now.
-        if (buttons[i].className.indexOf('show') !== -1) {
-            toggleCollapseButton({target: buttons[i].children[0]});
-        }
+        // Show all by default
+        toggleCollapseButton({target: buttons[i].children[0]});
     }
 
     var responseCodes = document.querySelectorAll('.example-names');
@@ -196,10 +194,8 @@ function init() {
         for (j = 0; j < tabButtons.length; j++) {
             tabButtons[j].onclick = toggleTabButton;
 
-            // Show by default?
-            if (j === 0) {
-                toggleTabButton({target: tabButtons[j]});
-            }
+            // Show all by default
+            toggleTabButton({target: tabButtons[j]});
         }
     }
 
